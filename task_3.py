c98de7f97547b9d8091a1f75bb65d8e6f72f244c
Sashakorepin14 <146105909+Sashakorepin14@users.noreply.github.com>
2023-12-19 15:27:12 +0000

    dfgabvb

--- task_3.py
+++ task_3.py
@@ -1,20 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def ellipse(N, a = 0.5, b = 0.2):
-    x = np.linspace(-1, 1, N)
-    y = np.linspace(-1, 1, N)
-    elips = (X**2)/(a**2) + (Y**2)/(b**2) - 1
+def plot_ellipse(a = 0.5, b = 0.2):
+    x = np.linspace(-1, 1, 100)
+    y = np.linspace(-1, 1, 100)
     
     X, Y = np.meshgrid(x, y)
+    
+    elips = (X**2)/(a**2) + (Y**2)/(b**2) - 1
     plt.contour(x, y, elips, levels=[0])
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('График эллипса')
     plt.axis('equal')
-    plt.grid(True)
     
     plt.savefig('fig_task_3.png')
     
 if __name__ == '__main__':
-    ellipse(100)
+    plot_ellipse()
