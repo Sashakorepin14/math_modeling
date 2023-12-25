9769dec0868bb3d53e544cdbff74483606d35e3d
Sashakorepin14 <146105909+Sashakorepin14@users.noreply.github.com>
2023-12-25 14:09:07 +0000

    fdfsdfd

--- task_4.py
+++ task_4.py
@@ -1,34 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_logarithmic_spiral(b = 4):
-    phi = np.linspace(0, 8*np.pi, 1000)
-    r = np.exp**(b * phi)
+def plot_logarithmic_spiral(b):
+    c = np.linspace(0, 8*np.pi, 1000)
+    r = 2.718 ** (b * c)
     
-    plt.polar(phi, r)
+    plt.polar(c, r)
     plt.title('Логарифмическая спираль')
     plt.savefig('fig_task_4_1.png')
 
-def plot_archimedean_spiral(k = 1):
-    phi = np.linspace(0, 8*np.pi, 1000)
-    r = k * phi
-    
-    plt.polar(phi, r)
+plot_logarithmic_spiral(0.1)
+
+def plot_arhimedova_spiral(k):
+    c = np.linspace(0, 8*np.pi, 1000)
+    r = k * c
+
+    plt.polar(c, r)
     plt.title('Архимедова спираль')
     plt.savefig('fig_task_4_2.png')
 
-def plot_staff_spiral(k = 2):
-    phi = np.linspace(0.01, 8*np.pi, 1000)
-    r = k / np.sqrt(phi)
-    
-    plt.polar(phi, r)
-    plt.title('Спираль «жезл»')
+plot_arhimedova_spiral(2)
+
+def plot_rod(k):
+    c = np.linspace(0.01, 8*np.pi, 1000)
+    r = k / (np.sqrt(c))
+
+    plt.polar(c, r)
+    plt.title('Жезл')
     plt.savefig('fig_task_4_3.png')
 
-def plot_polar_rose(k = 0.2):
-    phi = np.linspace(0, 2*np.pi, 1000)
-    r = np.sin(k * phi)
-    
-    plt.polar(phi, r)
+plot_rod(1)
+
+def plot_rose(k):
+    c = np.linspace(0.01, 8*np.pi, 1000)
+    r = np.sin(k * c)
+
+    plt.polar(c, r)
     plt.title('Роза')
     plt.savefig('fig_task_4_4.png')
+
+plot_rose(10)
