77b00325cdd7932f97bab16f4dbd2079ae15e27e
Sashakorepin14 <146105909+Sashakorepin14@users.noreply.github.com>
2024-01-11 14:47:04 +0000

    jhgmn

--- task_3.py
+++ task_3.py
@@ -2,34 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-
-def butterfly(t):
-    x = (np.sin(t)* (2,718 ** (np.cos(t)) - 2 * np.cos(4 * t) + (np.sin(t/12) ** 5))) 
-    y = (np.cos(t)* (2,718 ** (np.cos(t)) - 2 * np.cos(4 * t) + (np.sin(t/12) ** 5))) 
+def butterfly_equations(t):
+    x = 2 * np.cos(t) * (np.exp(np.cos(t)) - 2 * np.cos(4 * t) - np.sin(t/12)**5)
+    y = 2 * np.sin(t) * (np.exp(np.cos(t)) - 2 * np.cos(4 * t) - np.sin(t/12)**5)
     return x, y
 
-
-if __name__ == '__main__':
- 
-    fig, ax = plt.subplots()
-    hz, = ax.plot([], [], '-', color='r')
- 
-    plt.axis('equal')
-    ax.set_xlim(-25, 25)
-    ax.set_ylim(-25, 25)
-
-    def update(frame):
-        t =  frame * 0.1
-        x, y = butterfly(t) 
-        hz.set_data(x, y)
-        return hz,
-
-    
-    ani = FuncAnimation(fig,
-                        update,
-                        frames=100,
-                        interval=30
-                       )
-    
-	
-    ani.save('animation_4.gif') 
+ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
