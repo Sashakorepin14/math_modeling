e57663cc34035662c32a757d7f9c83b09afc3f6d
Sashakorepin14 <146105909+Sashakorepin14@users.noreply.github.com>
2024-01-16 17:01:07 +0000

    hgffg

--- project.py
+++ project.py
@@ -1,30 +1,55 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 
 
-fig = plt.figure()
-ax = plt.axes(xlim=(-100, 100), ylim=(-100, 100))
-circle, = ax.plot([], [], '-')
-plt.axis('equal')
+def circle_move(time, R=0, x0=0, y0=0, vy0=0):
+
+    x0 = x0
+    y0 = y0 + vy0 * time
+    alpha = np.arange(0, 3*np.pi, 0.1)
+    r = R * time ** 0.9
+
+    x = x0 + r*np.cos(alpha)
+    y = y0 + r*np.sin(alpha)
+    return x, y
+
+
+	
+def animate(i):
+    for j in range(len(balls)):
+        balls[j].set_data(circle_move(time=i, R=0.0005*r[j], x0=1*x[j], y0=0.01*y[j], vy0=0.01*vy[j]))
+
+
+if __name__ == '__main__':
  
-def init():
-    circle.set_data([], [])
-    return circle,
+    fig, ax = plt.subplots()
+    balls = []
+    for i in range(100):
+        ball, = plt.plot([], [], '-', color='b', label='Ball')
+        balls.append(ball)
+    
+    r = []
+    x = []
+    y = []
+    vy = []
+    for j in range(len(balls)):
+        r.append(random.randint(1, 99))
+        x.append(random.randint(1, 99))
+        y.append(random.randint(1, 99))
+        vy.append(random.randint(1, 99))
+    
 
-def update(t):
-    alpha = 0.5 
-    phi = np.linspace(0, 2 * np.pi, 1000)
-    R = alpha * t  
-    x = R * np.cos(phi)
-    for i in np.arange(-50, 50, 0.1):
-        y = R * np.sin(phi) + i
-    circle.set_data(x, y)
-    return circle,
-
-ani = FuncAnimation(fig, 
-                    update,
-                    frames=np.arange(0, 2*np.pi, 0.1),
-                    interval=40
-                    )
-ani.save('project.gif')
+    edge = 100
+    plt.axis('equal')
+    ax.set_xlim(0, edge)
+    ax.set_ylim(0, edge)
+    
+    ani = FuncAnimation(fig,
+                        animate,
+                        frames=200,
+                        interval=30
+                       )
+    	
+    ani.save('animation_4.gif') 
